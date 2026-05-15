7fdda2f3eaa573f148dfd92ef5becc7e7e094d0e
Rohit Kumar Mahto <rohitmahto062@gmail.com>
2026-05-15 18:42:29 +0530

    Add error handling and modal for weather fetch in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Container } from "react-bootstrap";
+import { Container, Modal } from "react-bootstrap";
 import Axios from "axios";
 import { useState } from "react";
 import ShowWeather from "./components/ShowWeather";
@@ -8,15 +8,30 @@ import Home from "./components/Home";
 function App() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState();
+  const [error, setError] = useState("");
+  const [smShow, setSmShow] = useState(false);
 
   const API_KEY = process.env.REACT_APP_API_KEY;
 
   const fetchWeather = async (e) => {
-    e.preventDefault()
-    let response = await Axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`,
-    );
-    setWeather(response.data)
+    try {
+      e.preventDefault();
+      let response = await Axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`,
+      );
+      setWeather(response.data);
+      setError("");
+      setSmShow(false)
+    } catch (error) {
+      setSmShow(true)
+      setError(
+        "Error while fetching weather!! Enter valid city name, eg  Delhi, Mumabi",
+      );
+
+      setTimeout(()=>{
+        setSmShow(false)
+      },3000)
+    }
   };
 
   const handleSearchAgain = () => {
@@ -51,6 +66,22 @@ function App() {
           )}
         </main>
       </Container>
+
+      {error && (
+        <Modal
+          size="sm"
+          show={smShow}
+          onHide={() => setSmShow(false)}
+          aria-labelledby="example-modal-sizes-title-sm"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="example-modal-sizes-title-sm" className="text-danger text-center">
+              ERROR!
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="text-black">{error}</Modal.Body>
+        </Modal>
+      )}
     </div>
   );
 }
